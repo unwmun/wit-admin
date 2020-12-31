2f77cbecb26f8412ebcbe462c9b75c04362d0299
unwmun <unwmun@gmail.com>
2020-12-31 17:10:39 +0900

    Dashboard 추가

---
+++ src/common/dashboard/index.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+
+const Dashboard = () => (
+    <Card>
+        <CardHeader title="Where is Treasure"/>
+        <CardContent>Where is Teasure의 관리자 페이지</CardContent>
+    </Card>
+)
+
+export { Dashboard };
