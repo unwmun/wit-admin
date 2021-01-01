c1fc05213c595206618e400907407cc7df8de783
unwmun <unwmun@gmail.com>
2021-01-01 23:00:03 +0900

    dataProvider 연결

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Admin } from 'react-admin';
 
+import dataProvider from "./dataprovider";
+
 // ui
 import { Dashboard } from "./common/dashboard";
 
 const App = () => (
-  <Admin dashboard={ Dashboard }>
+  <Admin dashboard={ Dashboard } dataProvider={ dataProvider }>
     
   </Admin>
 );
